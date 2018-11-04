852c4850740d2ffd96e07daa7674bd808c8812e8
Saul van der Walt <saulvdw@gmail.com>
2018-11-04 16:52:08 +0200

    that broke things, need another way to handle plain middleware

--- server.go
+++ server.go
@@ -26,6 +26,7 @@ var TheServer = &server{
 // serve starts the s.
 func (s *server) serve() error {
 	s.Server.Addr = Address
+	s.Server.Handler = s
 	s.Server.ReadTimeout = ReadTimeout
 	s.Server.ReadHeaderTimeout = ReadHeaderTimeout
 	s.Server.WriteTimeout = WriteTimeout
